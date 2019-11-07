3cda3e02c5d8e58e0fdd1ceb15f60398929609ec
Guilherme Kayo Shida <guilhermeshida@gmail.com>
2019-11-07 17:29:48 -0300

    Extract a QuizBrain class

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,8 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question({String q, bool a}) {
-    this.questionText = q;
-    this.questionAnswer = a;
+  Question(String q, bool a) {
+    questionText = q;
+    questionAnswer = a;
   }
 }
